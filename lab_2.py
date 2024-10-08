568a6041e266188c9a715aa7701ef4507a3fc6f5
S5727777 <s5727777@bournemouth.ac.uk>
2024-10-08 10:27:25 +0100

    Display Message Changed

--- lab_2.py
+++ lab_2.py
@@ -1,1 +1,24 @@
-print ("hello")
+
+number = 14
+number /=  7.0
+type_number = type(number)
+print(f"number = {number} and its data type is {type_number}")  
+ 
+a = 11
+b = 5
+result = a < b
+type_result =type(result)
+print(f"result = {result} and its data type is {type_result}")
+ 
+x = 15
+x= 43
+print({x})
+ 
+c = "exampletext"
+c = 23
+print(c)
+unit_name= "lab_2.py" # assign variable
+print("this unit is:", unit_name) # print variable
+print("The 1st character is:", unit_name[0]) # Access first character using index 0
+print("The 4th character is:", unit_name[3]) # 4th character is of index 4 -1 = 3
+print("The 9th character is:", unit_name[7]) # 9th character is of index 9 -1 = 8
