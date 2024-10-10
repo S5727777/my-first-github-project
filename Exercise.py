05bb643175514990e9ea539f91aea2022cbac023
Diego <S5727777@Bournemouth.ac.uk>
2024-10-10 12:14:26 +0100

    ecco a te

---
+++ Exercise.py
@@ -0,0 +1,13 @@
+
+x= "uanama"
+y= 10
+z= 5.5
+print(type(x))
+x= str(x)
+print(type(y))
+print(type(z))
+z=5.5
+z+=10
+type_z= type(z)
+result = 10+5.5
+print(f"z= {z}and its data ty[e is {type_z}")
--- Exploring_from_home.py
+++ Exploring_from_home.py
@@ -1,3 +1,70 @@
 print("this is a test")
-xy= 21
-print(xy*2)
+x= True 
+z= False 
+
+
+x1= 10 
+x= 14 
+print(x1) #Python will overwrite the most recent variable
+          #Therefore it may be good to specify the variable for example x1
+
+greeting_string= "Welcome to this test of programming from home"
+print (greeting_string)
+print("The 1st character is:", greeting_string[0])  
+print(greeting_string[0:5])
+print(greeting_string[-1:])
+print(greeting_string[-4:])
+a= 11
+print(type(a)) 
+a= 11.0
+print(type(a))
+a= "11"
+print(type(a))
+a= ("11"+"11")
+print(type(a))
+a= "a"
+print(type(a))
+a= True
+print(type(a))
+a= False
+print(type(a))
+x = 10.5
+print(type(x))
+x= int(x)
+print(type(x))
+x= 11 
+print(type(x))
+x= str(x)
+print(type(x))
+
+
+
+
+
+x= "uanama"
+y= 10
+z= 5.5
+print(type(x))
+x= str(x)
+print(type(y))
+print(type(z))
+z= "10"+"5.5"
+result = 10+5.5
+type_result= type(result)
+type_result(f"result= {result} and its data type is {type_result}")
+print(type(z))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
